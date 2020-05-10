0791f00d58006f2aed0879a953afb2624661e6c5
Vu.Tuan.Giang <giangvt-0992>
2020-05-10 22:01:26 +0700

    notification

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -9,6 +9,13 @@ class HomeController extends Controller
 {
     public function index()
     {
+        // $admin = getAuthAdmin();
+        //     $brand = $admin->brand;
+        //     $notifications = $brand->unreadNotifications()->where('data->type', 'ticket')->get();
+        //     echo('<pre>');
+        //     print_r($notifications);
+        //     echo('<pre>');
+        //     exit();
         return view('admin.home.index');
     }
 }
